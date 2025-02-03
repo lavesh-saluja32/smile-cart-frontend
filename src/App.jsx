eaa9130f6e3fc96a06000d8144f2346b462fc77d
admin <admin@admins-MacBook-Pro.local>
2025-02-03 18:23:52 +0530

    Setup NeetoUi

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
+import { Button } from "neetoui";
+
 import "./App.css";
-// eslint-disable-next-line import/extensions
-import logo from "./logo.svg";
+
+//eslint-disable-next-line import/extensions
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
+    <Button label="Click me" style="secondary" />
   </div>
 );
 
