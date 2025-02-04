7f79b26600e93cc9eb5217f77d3034b845281e9e
admin <admin@admins-MacBook-Pro.local>
2025-02-04 11:30:28 +0530

    Utilized updater function to set state values

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,13 +6,13 @@ import { Button } from "neetoui";
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const handleNext = () => {
-    const nextIndex = (currentIndex + 1) % imageUrls.length;
-    setCurrentIndex(nextIndex);
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
   };
 
   const handlePrevious = () => {
-    const previousIndex = (currentIndex - 1) % imageUrls.length;
-    setCurrentIndex(previousIndex);
+    setCurrentIndex(
+      prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
   };
 
   const handleIndex = key => {
