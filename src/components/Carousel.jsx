298ecf17b12172e088f8bb68af8fe2905293195d
admin <admin@admins-MacBook-Pro.local>
2025-02-04 11:28:42 +0530

    Added Carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
+
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handleNext = () => {
+    const nextIndex = (currentIndex + 1) % imageUrls.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePrevious = () => {
+    const previousIndex = (currentIndex - 1) % imageUrls.length;
+    setCurrentIndex(previousIndex);
+  };
+
+  const handleIndex = key => {
+    setCurrentIndex(key);
+  };
+
+  return (
+    <div className="flex items-center">
+      <Button
+        alt={title}
+        className="rink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Left}
+        style="text"
+        onClick={handlePrevious}
+      />
+      <div className="flex-col items-center justify-center">
+        <img
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <div className="flex w-[100%] justify-center">
+          {imageUrls.map((_ele, index) => (
+            // eslint-disable-next-line react/jsx-key
+            <div
+              key={index}
+              className={`h-3 w-3 cursor-pointer rounded-full border-2 border-black ${
+                currentIndex === index ? "bg-black" : "bg-white"
+              }`}
+              onClick={() => handleIndex(index)}
+            />
+          ))}
+        </div>
+      </div>
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Right}
+        style="text"
+        onClick={handleNext}
+      />
+    </div>
+  );
+};
+
+export default Carousel;
