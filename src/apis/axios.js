37622267b654a156b9cd4d3f1fe4d1628d00553d
admin <admin@admins-MacBook-Pro.local>
2025-02-05 11:39:30 +0530

    Implemented search functionality to product listing page

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,11 +1,19 @@
 import axios from "axios";
-import { keysToCamelCase } from "neetocist";
+import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { evolve } from "ramda";
 
 const transformResponseKeysToCamelCase = response => {
   if (response.data) response.data = keysToCamelCase(response.data);
 };
 
 const responseInterceptors = () => {
+  axios.interceptors.request.use(request =>
+    evolve(
+      { data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase },
+      request
+    )
+  );
+
   axios.interceptors.response.use(response => {
     console.log(response.data);
     transformResponseKeysToCamelCase(response);
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -3,15 +3,19 @@ import { useEffect, useState } from "react";
 import productsApi from "apis/products";
 import Header from "components/commons/Header";
 import PageLoader from "components/commons/PageLoader";
+import { Search } from "neetoicons";
+import { Input, NoData } from "neetoui";
+import { isEmpty } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
 const Home = () => {
+  const [searchKey, setSearchKey] = useState("");
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const fetchProducts = async () => {
     try {
-      const response = await productsApi.fetch();
+      const response = await productsApi.fetch({ searchTerm: searchKey });
       console.log(response.products, "pp");
       setProducts(response.products);
     } catch (error) {
@@ -23,7 +27,8 @@ const Home = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchKey]);
 
   if (isLoading) {
     return <PageLoader />;
@@ -31,12 +36,28 @@ const Home = () => {
 
   return (
     <div className="flex flex-col">
-      <Header shouldShowBackButton={false} title="Smile Cart" />
-      <div className="grid grid-cols-2 justify-items-center gap-y-8 md:grid-cols-3 lg:grid-cols-4">
-        {products.map(product => (
-          <ProductListItem key={product.slug} {...product} />
-        ))}
-      </div>
+      <Header
+        shouldShowBackButton={false}
+        title="Smile Cart"
+        actionBlock={
+          <Input
+            placeholder="Search products"
+            prefix={<Search />}
+            type="search"
+            value={searchKey}
+            onChange={event => setSearchKey(event.target.value)}
+          />
+        }
+      />
+      {isEmpty(products) ? (
+        <NoData className="h-full w-full" title="No products to show" />
+      ) : (
+        <div className="grid grid-cols-2 justify-items-center gap-y-8 md:grid-cols-3 lg:grid-cols-4">
+          {products.map(product => (
+            <ProductListItem key={product.slug} {...product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
